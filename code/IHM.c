f401ccd3b3454efa95f9a600a2ef334e0a94f247
xakp <gui.xak@gmail.com>
2013-06-04 11:54:08 +0200

    IHM terminee

--- code/IHM.c
+++ code/IHM.c
@@ -29,14 +29,20 @@
     int margex;                         /*!< en nombre de cellules de GROUND. */
     int margey;                         /*!< en nombre de cellules de GROUND. */
     
-    int dimsprite;                      /*!< dimention des sprite en pixels */
+    int wlvl;                           /*!< largeur du lvl en sprite */
+    int hlvl;                           /*!< hauteur du lvl en sprite. */
+    
+    int dimText;                        /*!< largeur du text en nombre de sprite. */
+    
+    int dimSprite;                      /*!< dimention des sprite en pixels */
     ALLEGRO_BITMAP* spritesheet;        /*!< pointeur sur la sprtiteshhep, globale mais proteges */
+    ALLEGRO_BITMAP** sprites;           /*!< tableau des sprites, globale mais proteges */
     
     ALLEGRO_EVENT_QUEUE *keyboardQueue; /*!< file pour les evenements clavier */
     ALLEGRO_EVENT_QUEUE *mouseQueue;    /*!< file pour les evenements sourie */
     ALLEGRO_EVENT_QUEUE *displayQueue;  /*!< file pour les evenements display */
     
-} ihm_context = {NULL, NULL, NULL, 0, 0, -1, NULL, NULL, NULL, NULL}; 
+} ihm_context = {NULL, NULL, NULL, 0, 0, 0, 0, 5, -1, NULL, NULL, NULL, NULL}; 
  
 
 
@@ -96,18 +102,44 @@ int ihm_init(int w, int h, int flags) {
  * \attention ne libere pas le lvl_t
  */
 void ihm_close() {
-    /* al_destroy_display(display); */
+    al_unregister_event_source(ihm_context.keyboardQueue, al_get_keyboard_event_source());
+    al_unregister_event_source(ihm_context.mouseQueue, al_get_mouse_event_source());
+    al_unregister_event_source(ihm_context.displayQueue, al_get_display_event_source(ihm_context.display));
+    
+	al_destroy_event_queue(ihm_context.keyboardQueue);
+	al_destroy_event_queue(ihm_context.mouseQueue);
+	al_destroy_event_queue(ihm_context.displayQueue);
+
+    al_destroy_display(ihm_context.display);
+    
+    /* desrtroy sprites */
 }
 
 
 
 /**
- * \fn int ihm_loadSpriteSheet(char* path, int dimsprite)
+ * \fn void ihm_drawBackgournd();
  * \brief 
  * \retval
  * 
  */
-int ihm_loadSpriteSheet(char* path, int dimsprite) {
+void ihm_drawBackgournd() {
+
+    al_set_target_backbuffer(ihm_context.display);
+    al_draw_bitmap(ihm_context.background, 0, 0, 0);
+    
+}
+
+
+
+/**
+ * \fn int ihm_loadSpriteSheet(char* path, int dimSprite)
+ * \brief 
+ * \retval
+ * 
+ */
+int ihm_loadSpriteSheet(char* path, int dimSprite) {
+    int i;
     
     /* Load and create bitmaps with an alpha channel */
     al_set_new_bitmap_format(ALLEGRO_PIXEL_FORMAT_ANY_32_WITH_ALPHA);
@@ -116,7 +148,22 @@ int ihm_loadSpriteSheet(char* path, int dimsprite) {
     ihm_context.spritesheet = al_load_bitmap(path);
     
     if (ihm_context.spritesheet != NULL ) {
-        ihm_context.dimsprite = dimsprite;
+        ihm_context.dimSprite = dimSprite;
+        
+        ihm_context.sprites = (ALLEGRO_BITMAP**) malloc( NBR_SPRITES * sizeof (ALLEGRO_BITMAP*) );
+        assert(  ihm_context.sprites );
+        
+        for (i=0 ; i<NBR_SPRITES ; i++) {
+            ihm_context.sprites[i] = 
+                al_create_sub_bitmap(   ihm_context.spritesheet, /* l'image parente */
+                                        ihm_context.dimSprite * ( i % 8 ), 
+                                        ihm_context.dimSprite * ( i / 8 ), 
+                                        ihm_context.dimSprite, ihm_context.dimSprite); /* largeur et hauteur */
+            
+            assert( ihm_context.sprites[i] != NULL );
+        }
+        
+        
         return (0);
     }
     return (-1);
@@ -130,12 +177,79 @@ int ihm_loadSpriteSheet(char* path, int dimsprite) {
  * \retval
  * 
  */
-void ihm_loadLab(lvl_t* lvl, int margex, int margey) {
+void ihm_loadLab(lvl_t* lvl, int margex, int margey, int dimText) {
+    int i, j, w, h, L;
+    
     ihm_context.lvl = lvl;
     ihm_context.margex = margex;
     ihm_context.margey = margey;
+    ihm_context.dimText = dimText;
     
-    /* resize en condition etc... */
+    ihm_context.wlvl = 0;
+    ihm_context.hlvl = 0;
+    
+    /* Largeur d'un sprite */
+    L = ihm_context.dimSprite;
+    
+    /* l'affiche dans le terminal */
+    for (i=0; lvl->dat[i] != NULL ; i++ ) {
+        for (j=0; lvl->dat[i][j] != lvl_NULL ; j++ );
+        ihm_context.wlvl = (j > ihm_context.wlvl) ? j : ihm_context.wlvl;
+    }
+    ihm_context.hlvl = i;
+    
+    /* en nombre de sprite */
+    w = ihm_context.wlvl + 2*(ihm_context.margex) + ihm_context.dimText;
+    h = ihm_context.hlvl + 2*(ihm_context.margey);
+    
+    /* resize */
+    al_resize_display( ihm_context.display, L*w, L*h );
+    
+    /* allou ll'image de fond */
+    if ( ihm_context.background != NULL )
+        al_destroy_bitmap( ihm_context.background );
+    
+    al_set_new_bitmap_flags( ALLEGRO_VIDEO_BITMAP ); /* carte grphique */
+    al_set_new_bitmap_format( ALLEGRO_PIXEL_FORMAT_ANY_32_WITH_ALPHA  );
+    ihm_context.background = al_create_bitmap(L*w, L*h);
+    
+    assert( ihm_context.background != NULL );
+    
+    /* creer l'image de fond qui ne change pas tout le long du niveau */
+    al_set_target_bitmap( ihm_context.background );
+    al_clear_to_color( al_map_rgb(0, 0, 0) );
+
+    
+    for (i=0; i<w ; i++ ) {
+        for (j=0; j<h ; j++ ) {
+            /* si on est dans le cadre */
+            if ( i<ihm_context.margex || i >= (ihm_context.margex + ihm_context.wlvl) 
+                || j<ihm_context.margey || j >= (ihm_context.margey + ihm_context.hlvl) ) {
+                
+                /* draw ground */
+                al_draw_bitmap( ihm_context.sprites[ ihm_GROUND ], i * L, j * L , 0);
+            }
+            
+            /* si on est dans le labyrinthe */
+            else {
+                switch (ihm_context.lvl->dat[j-ihm_context.margey][i-ihm_context.margex]) {
+                case lvl_WALL:
+                    /* draw wall */
+                    al_draw_bitmap( ihm_context.sprites[ ihm_WALL ], i * L, j * L , 0);
+                    break ;
+                case lvl_TARGET:
+                    /* draw target on ground */
+                    al_draw_bitmap( ihm_context.sprites[ ihm_GROUND ], i * L, j * L , 0);                
+                    al_draw_bitmap( ihm_context.sprites[ ihm_TARGET ], i * L, j * L , 0);
+                    break ;
+                default : 
+                    /* draw ground */
+                    al_draw_bitmap( ihm_context.sprites[ ihm_GROUND ], i * L, j * L , 0);
+                    
+                }/* ! switch */
+            }/* ! else */
+        }/* ! for j */
+    }/* ! for i */
 }
 
 
@@ -149,7 +263,7 @@ void ihm_loadLab(lvl_t* lvl, int margex, int margey) {
 int ihm_drawSpriteInLab(int posx, int posy, Sprites sp) {
     
     al_set_target_backbuffer(ihm_context.display);
-    al_draw_bitmap(ihm_context.spritesheet, 0, 0, 0);
+    al_draw_bitmap( ihm_context.sprites[ sp ], (posx+ihm_context.margex) * ihm_context.dimSprite,  (posy+ihm_context.margey) * ihm_context.dimSprite, 0);
     
     return 0;
 }
@@ -219,8 +333,8 @@ int mouseClicked(int* posx, int* posy) {
         al_get_next_event(ihm_context.mouseQueue, &event);
         
         if ( event.type == ALLEGRO_EVENT_MOUSE_BUTTON_UP ) {
-            *posx = (int)((event.mouse.x - (ihm_context.margex * ihm_context.dimsprite)) / ihm_context.dimsprite);
-            *posy = (int)((event.mouse.y - (ihm_context.margey * ihm_context.dimsprite)) / ihm_context.dimsprite);
+            *posx = (int)((event.mouse.x - (ihm_context.margex * ihm_context.dimSprite)) / ihm_context.dimSprite);
+            *posy = (int)((event.mouse.y - (ihm_context.margey * ihm_context.dimSprite)) / ihm_context.dimSprite);
             return (1);
         }
     }
--- code/IHM.h
+++ code/IHM.h
@@ -30,12 +30,12 @@ typedef int KEY_CODE;
  * 
  */
 typedef enum {  
-    ihm_PLAYER_LEFT = 0,        /*!<  */
-    ihm_PLAYER_RIGHT = 1,       /*!<  */
+    ihm_PLAYER_RIGHT = 0,        /*!<  */
+    ihm_PLAYER_LEFT = 1,       /*!<  */
     ihm_PLAYER_UP = 2,          /*!<  */
     ihm_PLAYER_DOWN = 3,        /*!<  */
-    ihm_PLAYER_LEFT_PUSH = 4,   /*!<  */
-    ihm_PLAYER_RIGHT_PUSH = 5,  /*!<  */
+    ihm_PLAYER_RIGHT_PUSH = 4,   /*!<  */
+    ihm_PLAYER_LEFT_PUSH = 5,  /*!<  */
     ihm_PLAYER_UP_PUSH = 6,     /*!<  */
     ihm_PLAYER_DOWN_PUSH = 7,   /*!<  */
     ihm_TARGET = 8,             /*!<  */
@@ -43,16 +43,19 @@ typedef enum {
     ihm_WALL = 10,              /*!<  */
     ihm_GROUND = 11             /*!<  */
 } Sprites;
+#define NBR_SPRITES 12
+
 
 
 /* initialise allegro ET le module clavier d'allegro, creer une pile en globale static ET lespiles d'events de display... */
-int ihm_init(int w, int h, int flags);
-void ihm_close();
+int     ihm_init(int w, int h, int flags);
+void    ihm_close();
 
 /* Fonctions relatives a l'affichage. */
-int         ihm_loadSpriteSheet(char* path, int dimsprite);
-void        ihm_loadLab(lvl_t* lvl, int margex, int margey);
-int         ihm_drawSpriteInLab(int posx, int posy, Sprites sp);
+int     ihm_loadSpriteSheet(char* path, int dimsprite);
+void    ihm_loadLab(lvl_t* lvl, int margex, int margey, int dimText);
+int     ihm_drawSpriteInLab(int posx, int posy, Sprites sp);
+void    ihm_drawBackgournd();
 
 
 /* Fonctions relatives aux evenements */
